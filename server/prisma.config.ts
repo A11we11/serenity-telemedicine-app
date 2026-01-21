e2e6f3c61977ecc6997fdc3eebefdfc4530f9863
0llwell <ogbonnaallwell1999@gmail.com>
2026-01-21 22:31:20 +0100

    still debugging

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -6,14 +6,3 @@ export default defineConfig({
     url: process.env.DATABASE_URL!,
   },
 });
-
-/* 
-import "dotenv/config";
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  schema: "./src/prisma/schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
-}); */
